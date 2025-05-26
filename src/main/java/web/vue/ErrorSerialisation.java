4995fa5dbc01c0f8a46342612a689192bfd721b9
EnyoYoen <enyotoyrep@gmail.com>
2025-05-26 15:39:24 +0200

    Forgot a file : ErrorSerialisation

---
+++ src/main/java/web/vue/ErrorSerialisation.java
@@ -0,0 +1,38 @@
+package web.vue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ErrorSerialisation extends Serialisation {
+    @Override
+    public void apply(HttpServletRequest request, HttpServletResponse response) {
+        PrintWriter out;
+        try {
+            out = response.getWriter();
+        } catch (IOException e) {
+            return;
+        }
+        response.setContentType("application/json;charset=UTF-8");
+
+        Map<String, Object> data;
+
+        data = new HashMap<>();
+        data.put("error", request.getAttribute("error"));
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(data);
+
+        out.println(json);
+
+        out.close();
+    }
+}
